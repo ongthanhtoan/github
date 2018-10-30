15790abedc3a07da56a474f04d43729b6f6fc4e3
ongthanhtoan <toanlh3@gmail.com>
2018-10-30 17:37:26 +0700

    abc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Thanh Toàn
+                    Ong Thanh Toàn
                 </div>
 
                 <div class="links">
